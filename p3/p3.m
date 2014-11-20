f59fbf4550947e4b7357a37055a1eb92f7ddd058
MichaGr <michael.groessler@web.de>
2014-11-20 15:48:38 +0100

    read input

---
+++ p3/p3.m
@@ -0,0 +1,13 @@
+% Authors: Michael Größler, Martin Zettwitz
+% programming task 3
+
+close all;clear;
+
+%get image
+[filename, pathname] = uigetfile('*.bmp', 'Select image');
+original = imread([pathname '/' filename]);
+clear pathname filename;
+
+%show original image
+imshow(original);
+title('input image');
