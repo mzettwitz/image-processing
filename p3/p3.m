c5eae4a6012aff8df69b8dc1e9747e1245572e12
MichaGr <michael.groessler@web.de>
2014-11-22 14:56:57 +0100

    wienerFilter, simple way
    used the simple way with 2 for loops
    easy, but slow
    TODO: get rid of'em

--- p3/p3.m
+++ p3/p3.m
@@ -1,18 +1,18 @@
-% Authors: Michael Größler, Martin Zettwitz
-% programming task 3
+## Authors: Michael Größler, Martin Zettwitz
+## programming task 3
 
 close all;clear;
 
-%get image
+#get image
 [filename, pathname] = uigetfile('*.bmp', 'Select image');
 original = imread([pathname '/' filename]);
 clear pathname filename;
 
-%show original image
+#show original image
 imshow(original);
 title('input image');
 
-%generate PSF
+#generate PSF
 
 
 
@@ -21,12 +21,13 @@ title('input image');
 
 
 
-%fft image and psf, reduce error
+#fft image and psf, reduce error
 
 
 
-%show corrected image 
+#show corrected image 
 
  
+#use wienerFilter to correct image, and show, find appropriate value of K
 
 
--- p3/wienerFilter.m
+++ p3/wienerFilter.m
@@ -1,6 +1,6 @@
 ##Author: Michael Größler, Martin Zettwitz
 
-function corImg = wienerFilter (errImg, PSF)
+function corImg = wienerFilter (errImg, PSF, K)
 
 #F(u,v) = ((H(u,v)^2/(H(u,v)*(|H(u,v)|^2+K))))*G(u,v)
 #where F is our corrected image
@@ -9,6 +9,11 @@ function corImg = wienerFilter (errImg, PSF)
 
 corImg = zeros(size(errImg,1),size(errImg,2));
 
-
+#easy but slow calculation, 
+for u= 1:size(corImg,1)
+  for v = 1:size(corImg,2)
+    corImg(u,v) += errImg(u,v) * ((PSF(u,v)^2)/(PSF(u,v)*(abs(PSF(u,v))^2 + K)));
+  end
+end
 
 endfunction
