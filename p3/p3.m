4165c41ca49a77831d596bd9a9805dccb129266a
Martin Zettwitz <00zetti@gmail.com>
2014-11-27 22:17:11 +0100

    -added function for psf with 45° and given pixel distance -inverse filtering

--- p3/p3.m
+++ p3/p3.m
@@ -13,20 +13,25 @@ imshow(original);
 title('input image');
 
 #generate PSF
+h = psf(original, 11);
+figure();
+imshow(h);
+title('psf');
 
+#transform image and psf
+F = fft2(fftshift(original));
+H = fft2(fftshift(h));
 
+%inverse image and psf
+G = F./H;
 
-
-
-
-
-
-#fft image and psf, reduce error
-
-
+%transform new image g into spatial room and show it
+g = ifftshift(ifft2(G));
 
 #show corrected image 
-
+figure();
+imshow(g);
+title('fft image and psf');
  
 #use wienerFilter to correct image, and show, find appropriate value of K
 
