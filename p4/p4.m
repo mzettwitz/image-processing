8349fc27d5a5d1afe683e7d3d99427973d5f6280
MichaGr <michael.groessler@web.de>
2014-12-09 16:51:18 +0100

    programming task 4 \{compressionrate}
    compression rate needs to be calculated,
    everything elde done
    interpretation needed

--- p4/p4.m
+++ p4/p4.m
@@ -13,16 +13,58 @@ imshow(original);
 title('input image');
 
 #2.seperate imag into M x M blocks(8|16)
+m =[8 16];
 
+# m(1) = 8 , m(2) = 16
+#create vectors of blocksize needed for mat2cell
+rows8 = ones(1,size(original,1)/m(1)) * m(1);
+cols8 = ones(1,size(original,2)/m(1)) * m(1);
+
+rows16 = ones(1,size(original,1)/m(2)) * m(2);
+cols16 = ones(1,size(original,2)/m(2)) * m(2);
+
+#blocksize of 8 x 8
+b8Image = mat2cell(original,rows8,cols8);
+
+#blocksize of 16x16
+b16Image = mat2cell(original,rows16,cols16);
 #3.DCT into frequency domain
-
+dct8Image = cellfun(@dct2,b8Image, 'Uniformoutput', false);
+dct16Image = cellfun(@dct2,b16Image, 'Uniformoutput', false);
 #4.quantization of frequencies
+load('quantm.mat');
 
+quant8Image = cellfun(@(x)  round(x ./quant8) , dct8Image, 'Uniformoutput', false);
+quant16Image = cellfun(@(x) round(x ./ quant16), dct16Image, 'Uniformoutput', false);
 #5.sort data(zig-zag) and encode them with run-length
+
+rle8Image = cellfun(@rle_enc,(cellfun(@zigzag, quant8Image, 'Uniformoutput', false)),'Uniformoutput', false);
+rle16Image = cellfun(@rle_enc,(cellfun(@zigzag, quant16Image, 'Uniformoutput', false)),'Uniformoutput', false);
+
+#calculate compressionrate
+
 
 #6.reverse the last steps and transform into spatial domain
 
+re8Image = cellfun(@zagzig,(cellfun(@rle_dec,rle8Image, 'Uniformoutput',false)),'Uniformoutput',false);
+re16Image = cellfun(@zagzig,(cellfun(@rle_dec,rle16Image, 'Uniformoutput',false)),'Uniformoutput',false);
+
+deQuant8Image = cellfun(@(x) x .* quant8 , re8Image, 'Uniformoutput', false);
+deQuant16Image = cellfun(@(x) x .* quant16 , re16Image, 'Uniformoutput', false);
+
+idct8Image = cellfun(@idct2,deQuant8Image, 'Uniformoutput', false);
+idct16Image = cellfun(@idct2,deQuant16Image, 'Uniformoutput', false);
+
+komp8Image = cell2mat(idct8Image);
+komp16Image = cell2mat(idct16Image);
 #7.show results for 8 and 16 blocks, show compression rate
 
+figure;
+imshow(komp8Image,[]);
+title(['M = 8 ']);
+
+figure;
+imshow(komp16Image,[]);
+title(['M = 16 ']);
 #answer to last question
 #(what are differences between block sizes and different pictures, why?)
