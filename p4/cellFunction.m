b3ec8dfb311488f2b76a3f3514c360dfdcdbb2f0
MichaGr <michael.groessler@web.de>
2014-12-14 10:58:08 +0100

    answered questions

--- p4/cellFunction.m
+++ p4/cellFunction.m
@@ -1,6 +1,7 @@
 ##Authors: Michael Groessler, Martin Zettwitz
 ##programming task : 4
 
+#just to shorten expression of cellfun
 function result = cellFunction (img, f = @func)
 
 result = cellfun(f,img,'Uniformoutput',false);
--- p4/p4.m
+++ p4/p4.m
@@ -9,10 +9,11 @@ original = imread([pathname '/' filename]);
 clear pathname filename;
 
 #show original image
+figure('Name','Input Image','NumberTitle','off');
 imshow(original);
-title('input image');
 
-#2.seperate imag into M x M blocks(8|16)
+
+##2.seperate imag into M x M blocks(8|16)
 m =[8 16];
 
 # m(1) = 8 , m(2) = 16
@@ -23,27 +24,28 @@ cols8 = ones(1,size(original,2)/m(1)) * m(1);
 rows16 = ones(1,size(original,1)/m(2)) * m(2);
 cols16 = ones(1,size(original,2)/m(2)) * m(2);
 
-#blocksize of 8 x 8
+#create blocks
 b8Image = mat2cell(original,rows8,cols8);
-
-#blocksize of 16x16
 b16Image = mat2cell(original,rows16,cols16);
-#3.DCT into frequency domain
+
+##3.DCT into frequency domain
 dct8Image = cellFunction(b8Image,@dct2);
 dct16Image = cellFunction(b16Image,@dct2);
-#4.quantization of frequencies
+
+##4.quantization of frequencies
 load('quantm.mat');
 
 quant8Image = cellFunction(dct8Image,@(x)  round(x ./quant8));
 quant16Image = cellFunction(dct16Image,@(x) round(x ./ quant16));
-#5.sort data(zig-zag) and encode them with run-length
 
+##5.sort data(zig-zag) and encode them with run-length
 rle8Image = cellFunction(cellFunction(quant8Image,@zigzag),@rle_enc);
 rle16Image = cellFunction(cellFunction(quant16Image,@zigzag),@rle_enc);
-#calculate compressionrate
 
-#use entries in original image(pixels) compared to entries in rle version
-#get rle for every cell , add up
+#calculate compressionrate
+#use number entries in original image(pixels) compared to entries in rle version
+#each entry represents the length of the according cell,
+#adding them up gives us the size of the particular cell
 rle8mat = cell2mat(cellFunction(rle8Image,@length));
 rle16mat = cell2mat(cellFunction(rle16Image,@length));
 
@@ -54,7 +56,7 @@ originalSize = size(original,1) * size(original,2);
 comp8Rate = originalSize/rle8Size;
 comp16Rate = originalSize/rle16Size;
 
-#6.reverse the last steps and transform into spatial domain
+##6.revert the last steps and transform into spatial domain
 
 re8Image = cellFunction(cellFunction(rle8Image,@rle_dec),@zagzig);
 re16Image = cellFunction(cellFunction(rle16Image,@rle_dec),@zagzig);
@@ -65,16 +67,36 @@ deQuant16Image = cellFunction(re16Image,@(x) x .* quant16);
 idct8Image = cellFunction(deQuant8Image,@idct2);
 idct16Image = cellFunction(deQuant16Image,@idct2);
 
-komp8Image = cell2mat(idct8Image);
-komp16Image = cell2mat(idct16Image);
+comp8Image = cell2mat(idct8Image);
+comp16Image = cell2mat(idct16Image);
+
 #7.show results for 8 and 16 blocks, show compression rate
 
-figure;
-imshow(komp8Image,[]);
-title(comp8Rate);
+figure('Name','M = 8','NumberTitle','off');
+imshow(comp8Image,[]);
+title(['Compressionrate = ' num2str(comp8Rate)]);
 
-figure;
-imshow(komp16Image,[]);
-title(comp16Rate);
+figure('Name','M = 16','NumberTitle','off');
+imshow(comp16Image,[]);
+title(['Compressionrate = ' num2str(comp16Rate)]);
 #answer to last question
 #(what are differences between block sizes and different pictures, why?)
+
+
+
+#M = 8
+#Das Bild ist kontrastschwächer als das Originalbild oder das M = 16 Bild
+#
+#M = 16
+#Dieses Bild weißt keine große Veränderung zum Original auf, 
+#Änderungen sind nur zu erkennen, wenn man ranzoomt
+#
+#Die höhere Kompressionsrate und die schlechtere Qualität des M = 8 Bildes
+#stehen im Zusammenhang mit der gewählten Quantisierungsmatrix. Da durch 
+#diese stärker quantisiert wird und mehr Werte zu 0 gesetzt werden, ist
+#das Bild dementsprechend schlechter.
+#Für M = 16 sind die Quantisierungskoeffizienten relativ klein, so dass
+#viele Frequenzen nahe des Mittelpunkts beibehalten werden. Dadurch sinkt
+#natürlich die Kompressionsrate. Allerdings bleibt die Bildqualität gut erhalten.
+#
+
